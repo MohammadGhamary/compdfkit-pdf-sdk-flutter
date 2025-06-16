5bc7ecc780bd8147275b8d5b28c619dcfffa267a
Mohammad <mohammadghamary76@outlook.com>
2025-06-16 04:26:48 +0330

    test password encryption

--- android/src/main/java/com/compdfkit/flutter/compdfkit_flutter/plugin/ComPDFKitSDKPlugin.java
+++ android/src/main/java/com/compdfkit/flutter/compdfkit_flutter/plugin/ComPDFKitSDKPlugin.java
@@ -227,6 +227,19 @@ public class ComPDFKitSDKPlugin extends BaseMethodChannelPlugin implements Plugi
         });
     }
 
+    private byte[] hexStringToBytes(String hex) {
+        int len = hex.length();
+        byte[] data = new byte[len / 2];
+
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte)
+                    ((Character.digit(hex.charAt(i), 16) << 4)
+                            + Character.digit(hex.charAt(i + 1), 16));
+        }
+
+        return data;
+    }
+
     private byte[] utf8ToHexBytes(String input, boolean havePadding) {
         StringBuilder hex = new StringBuilder();
         for (byte b : input.getBytes(StandardCharsets.UTF_8)) {
